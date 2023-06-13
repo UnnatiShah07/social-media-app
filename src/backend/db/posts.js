f6f4ddc4fbd51f358f635702db3556c948801be7
UnnatiShah07 <76685572+UnnatiShah07@users.noreply.github.com>
2023-06-13 21:48:40 +0530

    add: data in db

--- src/backend/db/posts.js
+++ src/backend/db/posts.js
@@ -1,4 +1,4 @@
-import { v4 as uuid } from "uuid";
+// import { v4 as uuid } from "uuid";
 import { formatDate } from "../utils/authUtils";
 
 /**
@@ -7,23 +7,126 @@ import { formatDate } from "../utils/authUtils";
  * */
 
 export const posts = [
+  // {
+  //   _id: uuid(),
+  //   content:
+  //     "Life isn’t about finding yourself. Life is about creating yourself.",
+  //   artImage:
+  //     "https://i.etsystatic.com/32281859/r/il/5e05cf/4565358316/il_794xN.4565358316_c5oj.jpg",
+  //   likes: {
+  //     likeCount: 0,
+  //     likedBy: [],
+  //     dislikedBy: [],
+  //   },
+  //   username: "shubhamsoni",
+  //   createdAt: formatDate(),
+  //   updatedAt: formatDate(),
+  // },
   {
-    _id: uuid(),
+    _id: "d8bc0df5-0ad5-4671-b603-92d89d50f57a",
     content:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
+      "Life isn’t about finding yourself. Life is about creating yourself.",
+    artImage:
+      "https://i.etsystatic.com/32281859/r/il/5e05cf/4565358316/il_794xN.4565358316_c5oj.jpg",
     likes: {
       likeCount: 0,
       likedBy: [],
       dislikedBy: [],
     },
-    username: "adarshbalika",
+    username: "shubhamsoni",
     createdAt: formatDate(),
     updatedAt: formatDate(),
   },
   {
-    _id: uuid(),
-    content:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
+    _id: "61b04ca0-0a26-4691-addd-b4c181537cb7",
+    content: "Faith it till you make it ✨",
+    artImage:
+      "https://i.etsystatic.com/18730211/r/il/a66238/1811347054/il_794xN.1811347054_f3wv.jpg",
+    likes: {
+      likeCount: 0,
+      likedBy: [],
+      dislikedBy: [],
+    },
+    username: "shubhamsoni",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+  },
+  {
+    _id: "baa50fb3-695c-48f3-a0de-94702593bf4a",
+    content: "Ganeshji 💗❤️",
+    artImage:
+      "https://i.etsystatic.com/17944019/r/il/93e029/4358382275/il_794xN.4358382275_bc6h.jpg",
+    likes: {
+      likeCount: 0,
+      likedBy: [],
+      dislikedBy: [],
+    },
+    username: "shubhamsoni",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+  },
+  {
+    _id: "925147ac-ffd6-4144-a0c6-8106a5a86ac3",
+    content: "Flower frame",
+    artImage:
+      "https://i.etsystatic.com/24069689/r/il/5d9815/4872856684/il_794xN.4872856684_l7g9.jpg",
+    likes: {
+      likeCount: 0,
+      likedBy: [],
+      dislikedBy: [],
+    },
+    username: "shubhamsoni",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+  },
+  {
+    _id: "1b164fd7-318c-4e56-81b5-e17ed0da0f94",
+    content: "Simple Mandala",
+    artImage:
+      "https://miro.medium.com/v2/resize:fit:720/format:webp/1*zAAChLy7byQiMqDzJcrHeg.jpeg",
+    likes: {
+      likeCount: 0,
+      likedBy: [],
+      dislikedBy: [],
+    },
+    username: "shubhamsoni",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+  },
+  {
+    _id: "563e92d9-554f-4928-88da-92306fc15791",
+    content: "Basuri Mandala",
+    artImage: "https://i.ytimg.com/vi/5CDyFMMHXLw/maxresdefault.jpg",
+    likes: {
+      likeCount: 0,
+      likedBy: [],
+      dislikedBy: [],
+    },
+    username: "shubhamsoni",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+  },
+  {
+    _id: "1d83464a-165a-4002-bb30-c6f1419ae746",
+    content: "Handmade polymer clay earrings",
+    artImage:
+      "https://i.etsystatic.com/8440299/r/il/db836f/2991092446/il_794xN.2991092446_tbpz.jpg",
+    likes: {
+      likeCount: 0,
+      likedBy: [],
+      dislikedBy: [],
+    },
+    username: "shubhamsoni",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+  },
+  {
+    _id: "a0eee344-13f6-481f-a8ea-a3a581f97c4c",
+    content: "Black Handmade polymer clay earrings",
+    artImage:
+      "https://i.etsystatic.com/19943978/r/il/f025ba/3378630748/il_794xN.3378630748_gcle.jpg",
+    artImageTwo:
+      "https://i.etsystatic.com/15599644/r/il/a8caa1/4560343544/il_794xN.4560343544_qk84.jpg",
     likes: {
       likeCount: 0,
       likedBy: [],
